b7aef850fa86c756669e604ec8932ce2341f6871
Ethan Lindley <ethanlindley@null.net>
2018-05-22 17:02:52 -0500

    general: add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='ByteArray.py',
+      version='1.0',
+      description='Simple library for reading and writing bytes in Python 2.7+ and Python 3.4+',
+      author='Zaseth',
+      url='https://github.com/Zaseth/ByteArray.py',
+      py_modules=['ByteArray'],
+     )
